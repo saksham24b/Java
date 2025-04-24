a7f67cf9d37a43e775a82efa80d895670e5a7750
Saksham <saksham24b@gmail.com>
2025-04-24 17:45:56 +0530

    clossed io scanner

--- Greet.java
+++ Greet.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Greet{
 
 	public static void main(String[] args){
-		var io = new Scanner(System.in);
-		System.out.println("What is your name? ");
-		var name = io.nextLine();
-		System.out.println("Hello, " + name + ".");
+		try(var io = new Scanner(System.in)){
+			System.out.println("What is your name? ");
+			var name = io.nextLine();
+			System.out.println("Hello, " + name + ".");
+		}
 	}
 }
